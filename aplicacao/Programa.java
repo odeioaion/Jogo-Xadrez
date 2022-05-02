b459dd159fa81f22a71227df43e0249c73bccba8
Bruno Reis <brunoreis_753@outlook.com>
2022-05-02 17:24:55 -0300

    começando a implementação do tabuleiro e das peças

--- aplicacao/Programa.java
+++ aplicacao/Programa.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import jogo_tabuleiro.Posicao;
+import jogo_tabuleiro.Tabuleiro;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Que comecem os Jogos");
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
 	}
 
 }
